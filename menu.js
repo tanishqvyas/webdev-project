6474277be5c63dd8d7679a00301e7541c7937604
shreya_pr <46067084+shreyapr6@users.noreply.github.com>
2019-09-29 22:14:03 +0530

    New design of menu bar

---
+++ menu.js
@@ -0,0 +1,8 @@
+function openSlideMenu()
+		{
+			document.getElementById('side-menu').style.width = '350px';
+		}
+function closeSlideMenu()
+		{
+			document.getElementById('side-menu').style.width = '0';
+		}
