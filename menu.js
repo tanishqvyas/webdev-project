7c0716bfc8d2e72049d0070168c6c56dbe44f2c2
tanishq vyas <tanishqvyas069@gmail.com>
2019-10-01 19:31:52 +0530

    Navigation introduced

--- menu.js
+++
@@ -1,8 +0,0 @@
-function openSlideMenu()
-		{
-			document.getElementById('side-menu').style.width = '350px';
-		}
-function closeSlideMenu()
-		{
-			document.getElementById('side-menu').style.width = '0';
-		}
