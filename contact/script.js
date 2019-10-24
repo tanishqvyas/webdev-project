a20a6dfa46f800f7f9fc81582c892207d65179e8
tanishqvyas <tanishqvyas069@gmail.com>
2019-10-24 21:19:49 +0530

    Css on name and smooth transitions on general petcare

--- contact/script.js
+++ contact/script.js
@@ -1,7 +1,7 @@
 function generate(){
     var pet_names = ['Kayla','Miko','Violet','Dottie','Maddie','Schotzie','Moochie','Snoop','Susie','Abbie','Mickey','Presley','Charles','Edsel','Peter','Norton','Munchkin',
         'Nana','Koba','Hobbes','Coal','Luna','Linus','Flopsy','Missie','Dash','Harry','Star','Fluffy','Sabine','Bibbles','Koty','Jaguar','Pinky','Sunny','Shasta','Roxy',
-        'Whitie','Bo','Dixie','Ebony','Rover','BJ','Wrinkles','Doogie','Justice','Pongo','Goldie','China','Tippy'];
+        'Whitie','Bo','Dixie','Ebony','Rover','BJ','Wrinkles','Doogie','Justice','Pongo','Goldie','China','Tippy','Caesar','Norbert','Charla','Mimky','Burpy','Chime','Luzlo','Patrick','Bruno','Scooby'];
     
     var rand_num = Math.floor(Math.random() * (pet_names.length) );
     document.getElementById('result').innerHTML = pet_names[rand_num]; 
@@ -9,4 +9,10 @@ function generate(){
 
 }
 
+
+
+function onfocusFun() {
+	
+	document.querySelector(".button").style.border = "none";
+}
 
