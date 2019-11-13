d2d932c0b8b8729225fb0af35243fde1064992d3
tanishqvyas <tanishqvyas069@gmail.com>
2019-11-13 22:04:30 +0530

    Prompt added in javascript for adopt page

--- adopt/adopt.js
+++ adopt/adopt.js
@@ -1,12 +1,18 @@
 // making a variable for submit button
-const btn = document.querySelector('.button');
+const btn = document.querySelectorAll('.button');
 const popUp = document.getElementById('successful-submit');
 
 // Adding event listner for the submit button
-btn.addEventListener('click', onsubmit);
+for (var i = btn.length - 1; i >= 0; i--) {
+	btn[i].addEventListener('click', onsubmit);
+}
+
 
 function onsubmit(event) 
 {
+	var personName = prompt("Please enter your Name");
+	var personNumber = prompt("Please enter your contact number");
+
 	event.preventDefault();
 	popUp.style.opacity = 1;
 
